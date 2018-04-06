f4921bb05f222c29a679d94b62b1c661eacbe3ff
Luis Deschamps Rudge <luis@luisrudge.net>
2018-04-06 16:39:42 -0300

    test app

---
+++ app.js
@@ -0,0 +1,116 @@
+//this is not related to Auth0. It's just here to make the page work correctly.
+Prism.plugins.NormalizeWhitespace.setDefaults({
+  'remove-trailing': true,
+  'remove-indent': true,
+  'left-trim': true,
+  'right-trim': true
+});
+var logs = [];
+if (window.localStorage.auth0logs) {
+  logs = JSON.parse(window.localStorage.auth0logs);
+}
+function printLogs() {
+  window.localStorage.auth0logs = JSON.stringify(logs);
+  var code = '';
+  logs.forEach(function(l) {
+    code += '\r\n' + JSON.stringify(l, null, 1);
+  });
+  var html = Prism.highlight(code, Prism.languages.javascript);
+  $('#logger').html(html);
+}
+function clearLogs() {
+  logs = [];
+  window.localStorage.removeItem('auth0logs');
+  printLogs();
+}
+function subscribeToEvents(instance) {
+  var validEvents = [
+    'show',
+    'hide',
+    'unrecoverable_error',
+    'authenticated',
+    'authorization_error',
+    'hash_parsed',
+    'signin ready',
+    'signup ready',
+    'forgot_password ready',
+    'socialOrPhoneNumber ready',
+    'socialOrEmail ready',
+    'vcode ready',
+    'forgot_password submit',
+    'signin submit',
+    'signup submit',
+    'socialOrPhoneNumber submit',
+    'socialOrEmail submit',
+    'vcode submit',
+    'federated login'
+  ];
+  validEvents.forEach(function(e) {
+    instance.on(e, function() {
+      var args = arguments;
+      if (arguments.length === 1) {
+        args = arguments[0];
+      }
+      logs.push({ event: e, arguments: args });
+      printLogs();
+    });
+  });
+}
+
+const clientId = '6qgR882b0vAiuTwsI6NZC9zynrUVF0mQ';
+const domain = 'auth.brucke.club';
+const defaultOptions = {
+  allowShowPassword: true,
+  usernameStyle: 'email',
+  defaultDatabaseConnection: 'acme',
+  prefill: {
+    email: 'johnfoo@gmail.com'
+  },
+  passwordlessMethod: 'code',
+  allowedConnections: ['email', 'acme', 'twitter']
+};
+function initLock() {
+  var lock = new Auth0Lock(clientId, domain, defaultOptions);
+  window.localStorage.lastUsed = 'lock';
+  subscribeToEvents(lock);
+  return lock;
+}
+function initPasswordless() {
+  var lockPasswordless = new Auth0LockPasswordless(clientId, domain, defaultOptions);
+  window.localStorage.lastUsed = 'passwordless';
+  subscribeToEvents(lockPasswordless);
+  return lockPasswordless;
+}
+$(function() {
+  printLogs();
+  $('#btn-clear-log').on('click', clearLogs);
+  $('#btn-show-lock').on('click', function() {
+    clearLogs();
+    var lock = initLock();
+    lock.show({
+      languageDictionary: {
+        title: 'Lock'
+      }
+    });
+  });
+  $('#btn-show-passwordless').on('click', function() {
+    clearLogs();
+    var lockPasswordless = initPasswordless();
+    lockPasswordless.show({
+      languageDictionary: {
+        title: 'Passwordless'
+      }
+    });
+  });
+  //make sure we initialize Lock so we can parse the hash
+  var lastUsed = window.localStorage.lastUsed;
+  if (!lastUsed) {
+    return;
+  }
+  if (lastUsed === 'lock') {
+    initLock();
+  } else {
+    initPasswordless();
+  }
+  window.localStorage.removeItem('lastUsed');
+});

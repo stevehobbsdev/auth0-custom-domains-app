d3df13a1c4f8db2df31d671383adacd86834faab
Luis Deschamps Rudge <luis@luisrudge.net>
2018-04-10 17:44:43 -0300

    no ls

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ Prism.plugins.NormalizeWhitespace.setDefaults({
   'right-trim': true
 });
 var logs = [];
-if (window.localStorage.auth0logs) {
-  logs = JSON.parse(window.localStorage.auth0logs);
-}
+// if (window.localStorage.auth0logs) {
+//   logs = JSON.parse(window.localStorage.auth0logs);
+// }
 function printLogs() {
-  window.localStorage.auth0logs = JSON.stringify(logs);
+  //window.localStorage.auth0logs = JSON.stringify(logs);
   var code = '';
   logs.forEach(function(l) {
     code += '\r\n' + JSON.stringify(l, null, 1);
@@ -20,7 +20,7 @@ function printLogs() {
 }
 function clearLogs() {
   logs = [];
-  window.localStorage.removeItem('auth0logs');
+  //window.localStorage.removeItem('auth0logs');
   printLogs();
 }
 function subscribeToEvents(instance) {
@@ -80,13 +80,13 @@ var auth0 = new auth0.WebAuth({
 });
 function initLock() {
   var lock = new Auth0Lock(clientId, domain, defaultOptions);
-  window.localStorage.lastUsed = 'lock';
+  //window.localStorage.lastUsed = 'lock';
   subscribeToEvents(lock);
   return lock;
 }
 function initPasswordless() {
   var lockPasswordless = new Auth0LockPasswordless(clientId, domain, defaultOptions);
-  window.localStorage.lastUsed = 'passwordless';
+  //window.localStorage.lastUsed = 'passwordless';
   subscribeToEvents(lockPasswordless);
   return lockPasswordless;
 }
@@ -113,14 +113,15 @@ $(function() {
   });
   $('#btn-ulp').on('click', function() {
     clearLogs();
-    window.localStorage.lastUsed = 'a0js';
+    //window.localStorage.lastUsed = 'a0js';
     auth0.authorize();
   });
   //make sure we initialize Lock so we can parse the hash
-  var lastUsed = window.localStorage.lastUsed;
-  if (!lastUsed) {
-    return;
-  }
+  //var lastUsed = window.localStorage.lastUsed;
+  // if (!lastUsed) {
+  //   return;
+  // }
+  return;
   switch (lastUsed) {
     case 'lock':
       initLock();
